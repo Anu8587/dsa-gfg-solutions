7e61d4fd2c6a7057c86889090ff6ec00728ab4d3
Anushka Singh <155012947+Anu8587@users.noreply.github.com>
2026-02-19 15:16:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int countFreq(vector<int>& arr, int target) {
+        return upper_bound(arr.begin(), arr.end(), target) -
+               lower_bound(arr.begin(), arr.end(), target);
+    }
+};
+
